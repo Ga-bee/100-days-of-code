923a7d01641dacad9bf02497e1b9078ffae5297a
Ga-bee <gabrieladealmeidajorge@hotmail.com>
2023-03-21 22:19:53 -0300

    Create Ex_05_01.py

---
+++ Day 05/Ex_05_01.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+soma = 0
+for items in student_heights:
+    soma += items
+
+media = round(soma/len(student_heights))
+ 
+print(media)

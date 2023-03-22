7a73198eb2ecf9b6cc15a29b4fec243cb5cb8a90
Ga-bee <gabrieladealmeidajorge@hotmail.com>
2023-03-21 23:37:57 -0300

    Update Password_generator.py

--- Day 05/Password_generator.py
+++ Day 05/Password_generator.py
@@ -1,2 +1,12 @@
-print('Welcome to th Pypassword Generator!')
+print('Welcome to the Pypassword Generator!')
+letters_list = ['a','b', 'c','d','e','f','g','h','i','j','k','l','m','n', 'o', 'p', 'q','r', 's','t','u', 'v','w', 'x','y','z']
+symbols_list = ['!','@','$','%','&','*','(',')']
+numbers = int(input('How many numbers would you like in your password. \n'))
+symbol = int(input('How many symbols would you like in your password. \n'))
+letters = int(input('How many letters would you like in your password. \n'))
 
+# for letter in letters_list:
+
+#password = 
+
+print(f'Here is your password:', end='')

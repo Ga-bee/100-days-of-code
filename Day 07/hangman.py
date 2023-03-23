b7c9621e5fb7eec58f1d4433384e4771a55d3cd2
Ga-bee <gabrieladealmeidajorge@hotmail.com>
2023-03-23 16:02:22 -0300

    finished day 7

--- Day 07/hangman.py
+++ Day 07/hangman.py
@@ -1,27 +1,104 @@
+##### Terminar e deixar mais bonitin
+from word_list import words_list
+from ascii_hangman import HANGMANPICS
 import random
-word_list = ['apple','pie','banana','word','piece','computer', 'run','dress','golf'] 
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(words_list)
 
 display =[]
 
+for c in range(len(chosen_word)):
+    display += '_'
 
-while True: #len(display) != len(chosen_word):    
+lifes = 6
+
+def hanged(life):
+    print('')
+    for i in range(life):
+            print('''<3''', end ='')
+    print(f'\n\nLifes: {lifes}')
+    if life == 6:
+        print(HANGMANPICS[0])
+    if life == 5:
+        print(HANGMANPICS[1])
+    if life == 4:
+        print(HANGMANPICS[2])
+    if life == 3:
+        print(HANGMANPICS[3])
+    if life == 2:
+        print(HANGMANPICS[4])
+    if life == 1:
+        print(HANGMANPICS[5])
+    if life == 0:
+        print(HANGMANPICS[6], '_' * 30)
+
+print('''                                                                                           
+▀████▀  ▀████▀▀     ██     ▀███▄   ▀███▀ ▄▄█▀▀▀█▄█▀████▄     ▄███▀     ██     ▀███▄   ▀███▀
+  ██      ██       ▄██▄      ███▄    █ ▄██▀     ▀█  ████    ████      ▄██▄      ███▄    █  
+  ██      ██      ▄█▀██▄     █ ███   █ ██▀       ▀  █ ██   ▄█ ██     ▄█▀██▄     █ ███   █  
+  ██████████     ▄█  ▀██     █  ▀██▄ █ ██           █  ██  █▀ ██    ▄█  ▀██     █  ▀██▄ █  
+  ██      ██     ████████    █   ▀██▄█ ██▄    ▀████ █  ██▄█▀  ██    ████████    █   ▀██▄█  
+  ██      ██    █▀      ██   █     ███ ▀██▄     ██  █  ▀██▀   ██   █▀      ██   █     ███  
+▄████▄  ▄████▄▄███▄   ▄████▄███▄    ██   ▀▀███████▄███▄ ▀▀  ▄████▄███▄   ▄████▄███▄    ██  
+                                                                                           
+                                                                                           
+''')
+
+while True:
+    hanged(lifes)
     guess = input('Guess a letter:\n').lower()
+
+    if guess not in chosen_word:
+        lifes -= 1
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
-        if guess == chosen_word[position]:
+
+        if guess == letter:
             display[position] = letter
-            print(display)
-        if guess!= chosen_word[position]:
-            display += '_'
-            print(display)
-        
-    if '-' not in display:
+
+
+        if display[position] != '_':
+            display[position] = display[position]
+    answer = ''.join(display)        
+    print(answer)
+    if lifes == 0:
+        print(''' ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄    ▄▄▄     ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ 
+█  █ █  █       █  █ █  █  █   █   █       █       █       █       █
+█  █▄█  █   ▄   █  █ █  █  █   █   █   ▄   █   ▄   █  ▄▄▄▄▄█    ▄▄▄█
+█       █  █ █  █  █▄█  █  █   █   █  █ █  █  █ █  █ █▄▄▄▄▄█   █▄▄▄ 
+█▄     ▄█  █▄█  █       █  █   █▄▄▄█  █▄█  █  █▄█  █▄▄▄▄▄  █    ▄▄▄█
+  █   █ █       █       █  █       █       █       █▄▄▄▄▄█ █   █▄▄▄ 
+  █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█  █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█
+''')
+        print(f'The word is: {chosen_word}!')
+        break
+    if len(display) > 0 and '_' not in display:
+        print(''' ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄    ▄     ▄ ▄▄▄ ▄▄    ▄ 
+█  █ █  █       █  █ █  █  █ █ ▄ █ █   █  █  █ █
+█  █▄█  █   ▄   █  █ █  █  █ ██ ██ █   █   █▄█ █
+█       █  █ █  █  █▄█  █  █       █   █       █
+█▄     ▄█  █▄█  █       █  █       █   █  ▄    █
+  █   █ █       █       █  █   ▄   █   █ █ █   █
+  █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█  █▄▄█ █▄▄█▄▄▄█▄█  █▄▄█
+''')
+        print(f'The word is {chosen_word}')
         break
 
-print('')
-print(display)
-# while finished == 0:
+# while True: #len(display) != len(chosen_word):    
+#     guess = input('Guess a letter:\n').lower()
+#     for position in range(len(chosen_word)):
+#         letter = chosen_word[position]
+#         if guess == chosen_word[position]:
+#             display[position] = letter
+            
+#         if guess!= chosen_word[position]:
+#             display += '_'
+        
+#     if '-' not in display:
+#         break
+
+# print('')
+# print(display)
+# # while finished == 0:
 #     guess = input('Guess a letter\n')
 
 #     for c in range(len(choosen_word)):
@@ -33,5 +110,4 @@ print(display)
     
 #     print('')
 
-# print('')
-print(chosen_word)
+# # print('')
---
+++ Day 07/word_list.py
@@ -0,0 +1,17 @@
+words_list = ['show','costume','second', 'hour', 'car','apple','pie','banana','word','piece','computer', 'run', 'wombat' ,
+             'dress','golf','library','satisfying','billowy','disagreeable', 'bounce','extend','drain','healthy','wolf',
+             'arithmetic','unwritten','leather','draconian', 'Gym','Grow','Muddy','Furnished','Tank','Rib','Hungarian',
+             'Pesto','Hop','Slack','Cadmium','Stash','Ignition','Resignation','Lope','Ketchup','Malleable','fan','turtle', 
+             'ant','baboon',  'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'clam', 'cobra', 'cougar', 'frog','goat',
+             'coyote', 'crow', 'deer', 'dog', 'donkey', 'duck', 'eagle', 'ferret', 'fox','mouse','mule', 'newt','raven',
+             'goose', 'hawk', 'lion', 'lizard', 'llama', 'mole', 'monkey', 'moose',  'rabbit', 'ram', 'rat' ,'pigeon', 'python',
+             'otter' ,'owl' ,'panda' ,'parrot' ,'sheep' ,'skunk' ,'sloth', 'snake', 'spider', 'turkey',  'weasel', 'whale', 
+             'rhino' ,'salmon', 'seal', 'shark' , 'stork' ,'swan' ,'tiger', 'toad', 'trout' , 'zebra'
+              ] 
+
+
+ 
+
+ 
+
+ 

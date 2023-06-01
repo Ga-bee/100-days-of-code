3e57843c7045a03edf3d694ec5cabed514434fdd
Ga-bee <116361973+Ga-bee@users.noreply.github.com>
2023-06-01 17:05:54 -0300

    Update main.py
    jjjjjjj

--- Day 36/main.py
+++ Day 36/main.py
@@ -2,8 +2,8 @@ import requests as rq
 
 from twilio.rest import Client
 
-account_sid = 'AC2c242e76c2071e86643dab69e967eb01'
-auth_token = '64958d7b7c90be0b6999d1fc62dc65f5 '
+account_sid = '<>'
+auth_token = '<> '
 client = Client(account_sid, auth_token)
 
 
@@ -11,9 +11,9 @@ response = rq.get("https://cat-fact.herokuapp.com/facts")
 response.raise_for_status()
 facts = response.json()
 message = client.messages.create(
-  from_='whatsapp:+14155238886',
+  from_='whatsapp:+yourtwilionumber',
   body=f"Did you know?\n{facts[0]['text']} \nThat's today's cat fact.\nSee you tomorrow bringing more facts!",
-  to='whatsapp:+5522999440175'
+  to='whatsapp:+yournumber'
 )
 print(facts[0]["text"])
 print(message.sid)
